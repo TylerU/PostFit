5078223797b24823d0ee8b9287aca46d2a331f12
QuixoticVortex <dinoentrails@gmail.com>
2015-06-09 00:54:48 -0400

    tinkering

--- demodata/schools.js
+++ demodata/schools.js
@@ -4,13 +4,15 @@ module.exports = [
         state: "Iowa",
         conference: "North Iowa Conference",
         name: "Osage Green Devils",
-        enrollment: 200
+        enrollment: 200,
+        id: 1
     },
     {
         city: "Linn-Mar",
         state: "Iowa",
         conference: "Cedar Rapids Conference",
         name: "Lions",
-        enrollment: 2000
+        enrollment: 2000,
+        id: 2
     }
 ];
